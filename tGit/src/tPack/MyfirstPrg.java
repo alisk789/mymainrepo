52dd34cfb07c189a00eb98fe6a80c2738731701a
Bheemesh <Bheemesh@192.168.1.47>
2018-07-26 17:04:56 +0530

    prem commiting code

---
+++ tGit/src/tPack/MyfirstPrg.java
@@ -0,0 +1,15 @@
+package tPack;
+
+public class MyfirstPrg {
+
+	public static void main(String[] args)
+	
+	{
+		
+		System.out.println("welcome to git");
+		System.out.println("my First Prg");
+		System.out.println("my welcome to git");
+
+	}
+
+}
